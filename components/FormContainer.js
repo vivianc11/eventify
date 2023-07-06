4b14a1948c65a5b4f1dc8fb89f33034ddf21205b
Vivian Chen <vivian.chen1193@gmail.com>
2023-07-06 11:07:06 -0700

    started form components for login and sign up input

---
+++ components/FormContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FormContainer = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default FormContainer;
