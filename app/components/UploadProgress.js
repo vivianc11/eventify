3a43c3350f6f911dcaaccc362dd6ea28ef07e6b6
Vivian Chen <vivian.chen1193@gmail.com>
2023-08-21 10:24:57 -0700

    installed react native progress for progess circle for image upload

--- app/components/UploadProgress.js
+++ app/components/UploadProgress.js
@@ -1,12 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function UploadProgress() {
+import * as Progress from 'react-native-progress';
+
+export default function UploadProgress({ process }) {
   return (
-    <View>
-      <Text>UploadProgress</Text>
+    <View style={[StyleSheet.absoluteFillObject, styles.container]}>
+      <Progress.Circle
+        progress={process}
+        width={200}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 1,
+      backgroundColor: 'rgba(142, 154, 175, 0.3)'
+    },
+})
