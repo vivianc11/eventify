ed83d6a9761aeed7c116594a79e21568cb9a9f45
Vivian Chen <vivian.chen1193@gmail.com>
2023-08-21 13:35:00 -0700

    fixed bug with fetching user; changed isloggedin to setisloggedin

--- app/context/LoginProvider.js
+++ app/context/LoginProvider.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react"
 import client from "../api/client";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginContext = createContext();
 
@@ -12,36 +13,43 @@ const LoginProvider = ({ children }) => {
   // checking if device has a token stored, 
   // if token exists, then will be used to check for validation
   const fetchUser = async () => {
+    setLoginPending(true);
     const token = await AsyncStorage.getItem('token');
-    if(token !== null) {
+    if (token !== null) {
       const res = await client.get('/profile', {
         headers: {
           Authorization: `JWT ${token}`
         }
       })
-      if(res.data.success){
+      console.log('logged in as:', res.data)
+      if (res.data.success) {
         setProfile(res.data.profile);
-        isLoggedIn(true);
+        setIsLoggedIn(true);
       } else {
         setProfile({});
-        isLoggedIn(false);
+        setIsLoggedIn(false);
       }
+      setLoginPending(false);
+    } else {
+      setProfile({});
+      setIsLoggedIn(false);
+      setLoginPending(false);
     }
   }
 
   useEffect(() => {
     fetchUser();
-  },[])
+  }, [])
 
   return (
-    <LoginContext.Provider 
-      value={{ 
-        isLoggedIn, 
-        setIsLoggedIn, 
-        profile, 
+    <LoginContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        profile,
         setProfile,
         loginPending,
-        setLoginPending 
+        setLoginPending
       }}
     >
       {children}
@@ -49,6 +57,6 @@ const LoginProvider = ({ children }) => {
   )
 }
 
-export const useLogin = () => useContext(LoginContext); 
+export const useLogin = () => useContext(LoginContext);
 
 export default LoginProvider;
