96e2b1a957e0c2992651ce3886a40dd191d91bf5
Vivian Chen <vivian.chen1193@gmail.com>
2023-08-21 13:20:45 -0700

    added useEffect hook to invoke fetchUser function to check if there is a jwt token stored on user's device. If token exists, then user will be fetched from backend and logged in, if not, then user will be sent to login page to login

--- app/context/LoginProvider.js
+++ app/context/LoginProvider.js
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from "react"
+import React, { createContext, useContext, useEffect, useState } from "react"
+import client from "../api/client";
 
 const LoginContext = createContext();
 
@@ -7,6 +8,30 @@ const LoginProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [profile, setProfile] = useState({});
   const [loginPending, setLoginPending] = useState(false);
+
+  // checking if device has a token stored, 
+  // if token exists, then will be used to check for validation
+  const fetchUser = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if(token !== null) {
+      const res = await client.get('/profile', {
+        headers: {
+          Authorization: `JWT ${token}`
+        }
+      })
+      if(res.data.success){
+        setProfile(res.data.profile);
+        isLoggedIn(true);
+      } else {
+        setProfile({});
+        isLoggedIn(false);
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  },[])
 
   return (
     <LoginContext.Provider 
